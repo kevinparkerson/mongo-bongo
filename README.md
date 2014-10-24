c82923a26daa4fc75593d4cb688c1b34b8780ad7
kevinparkerson <kparkerson@exacttarget.com>
2014-10-24 11:25:24 -0400

    initial app going

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ mongo-bongo
 ===========
 
 Messing with that MongoDB
+
+### Get it Running
+
+* if you haven't already, install [node](http://nodejs.org/) and [grunt](http://gruntjs.com/)
+* run `npm install` in terminal
+* run `grunt install` in terminal
+* run `node app` or `node app-mc`
+* hit up 'localhost:3000/' in a browser
+* enjoy
---
+++ app-mc.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//Core Modules
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+//Basic Dependencies
+var express = require('express');
+var expressHanlebars = require( 'express-handlebars' );
+var helpers = require('./lib/hbs-helpers');
+var MongoClient = require('mongodb').MongoClient;
+
+//App Dependencies
+var routes = require('./mc-files/routes');
+
+//Creating express application & setting port
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+//Configuring view engine
+app.engine('hbs', expressHanlebars({
+	defaultLayout: 'main',
+	extname: '.hbs',
+	helpers: helpers
+	//partialsDir: __dirname +'/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+//Serving static content
+app.use(express.static(__dirname +'/public'));
+app.use('/bower_components', express.static(__dirname+'/bower_components'));
+
+//Use routes
+app.use('/', routes);
+
+//Start server
+var server = app.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
